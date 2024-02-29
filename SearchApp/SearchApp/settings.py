bf273431e71665d29902b54f900d9aa5ae5ea6ef
FWasik <franekwasik2@gmail.com>
2024-02-29 19:26:31 +0100

    Update settings.py

--- SearchApp/SearchApp/settings.py
+++ SearchApp/SearchApp/settings.py
@@ -26,15 +26,13 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-# ALLOWED_HOSTS = [
-#                 "localhost",
-#                 "127.0.0.1",
-#                 "0.0.0.0"
-#                 "http://search-app.eu-central-1.elasticbeanstalk.com/",
-# ]
-
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = [
+                "localhost",
+                "127.0.0.1",
+                "0.0.0.0",
+                "172.17.0.2",
+                "http://search-app.eu-central-1.elasticbeanstalk.com/",
+]
 
 
 # Application definition
